48303bad4ebc2bda9a910f9ab270e93fef975266
Andita Kaesar <anditakaesar@gmail.com>
2020-03-11 15:41:47 +0700

    build debug

--- build/index.js
+++ build/index.js
@@ -11,8 +11,10 @@ var _app = _interopRequireDefault(require("./app"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 // import app from './app';
-_app["default"].listen(_env.env.PORT, function () {
+_app["default"].listen(function () {
   if (_env.env.NODE_ENV === 'development') {
     _logger["default"].info("app listen at port ".concat(_env.env.PORT));
+  } else {
+    _logger["default"].info("env vars", _env.env);
   }
 });
--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import { env } from './env';
 import logger from './logger';
 import http from './app';
 
-http.listen(env.PORT, () => {
+http.listen(() => {
     if (env.NODE_ENV === 'development') {
         logger.info(`app listen at port ${env.PORT}`);
+    } else {
+        logger.info(`env vars`, env);
     }
 });
